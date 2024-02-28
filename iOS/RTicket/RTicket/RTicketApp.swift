f2a478bb8af847f1687c75851906cbc735e536fd
Carlos Kelley <carloskelley@protonmail.com>
2024-02-28 01:22:39 -0600

    feat: initial commit of local app

--- iOS/RTicket/RTicket/RTicketApp.swift
+++ iOS/RTicket/RTicket/RTicketApp.swift
@@ -8,8 +8,8 @@
 import SwiftUI
 import RealmSwift
 
-let realmApp = RealmSwift.App(id: "rticket-xxxxx") // TODO: Copy the app id from the backend Realm app
-let useEmailPasswordAuth = false // TODO: set to "true" if you want to user username/password rather than anonymous authentication
+let realmApp = RealmSwift.App(id: "rticket-waseh") // TODO: Copy the app id from the backend Realm app
+let useEmailPasswordAuth = true // TODO: set to "true" if you want to user username/password rather than anonymous authentication
 
 @main
 struct RTicketApp: SwiftUI.App {
